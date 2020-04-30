80c45c26bff2457660e356b8b0a356ec054d57ec
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-30 16:30:38 -0700

    finished with CartContext wrapping

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShoppingCart from "./components/ShoppingCart";
 
 //Context
 import { ProductContext } from "./contexts/productContext";
-import {CartContext} from "./contexts/cartContext";
+import { CartContext } from "./contexts/cartContext";
 
 function App() {
   const [products] = useState(data);
@@ -23,16 +23,18 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <CartContext.Provider value={{cart}}>
+          <Navigation cart={cart} />
 
-        {/* Routes */}
-        <Route exact path="/">
-          <Products />
-        </Route>
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
+          <Route path="/cart">
+            <ShoppingCart cart={cart} />
+          </Route>
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );

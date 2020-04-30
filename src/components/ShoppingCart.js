53b81c7b0296202b9757774e28debde66c6b241c
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-30 16:34:23 -0700

    created createContext hook

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,29 +1,33 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
 
 // Components
-import Item from './ShoppingCartItem';
+import Item from "./ShoppingCartItem";
 
-import {CartContext} from '../contexts/cartContext';
+import { CartContext } from "../contexts/cartContext";
 
-const ShoppingCart = props => {
-	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
-			return acc + value.price;
-		}, 0).toFixed(2);
-	};
+const ShoppingCart = (props) => {
+  const { cart } = useContext(CartContext);
 
-	return (
-		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
-			))}
+  const getCartTotal = () => {
+    return props.cart
+      .reduce((acc, value) => {
+        return acc + value.price;
+      }, 0)
+      .toFixed(2);
+  };
 
-			<div className="shopping-cart__checkout">
-				<p>Total: ${getCartTotal()}</p>
-				<button>Checkout</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="shopping-cart">
+      {props.cart.map((item) => (
+        <Item key={item.id} {...item} />
+      ))}
+
+      <div className="shopping-cart__checkout">
+        <p>Total: ${getCartTotal()}</p>
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingCart;
